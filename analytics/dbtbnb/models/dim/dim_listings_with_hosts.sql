46d28d043aedbe3a380e4aa82c67b49a180e82db
Stefan Langenbach <stefan@langenbach.online>
2025-11-05 15:55:39 +0100

    Run linters

--- analytics/dbtbnb/models/dim/dim_listings_with_hosts.sql
+++ analytics/dbtbnb/models/dim/dim_listings_with_hosts.sql
@@ -1,22 +1,22 @@
 WITH
-LISTINGS AS (
+l AS (
     SELECT * FROM {{ ref("dim_listings_cleansed") }}
 ),
 
-HOSTS AS (
+h AS (
     SELECT * FROM {{ ref("dim_hosts_cleansed") }}
 )
 
 SELECT
-    LISTINGS.LISTING_ID,
-    LISTINGS.LISTING_NAME,
-    LISTINGS.ROOM_TYPE,
-    LISTINGS.MINIMUM_NIGHTS,
-    LISTINGS.PRICE,
-    LISTINGS.HOST_ID,
-    HOSTS.HOST_NAME,
-    HOSTS.IS_SUPERHOST AS HOST_IS_SUPERHOST,
-    LISTINGS.CREATED_AT,
-    GREATEST(LISTINGS.UPDATED_AT, HOSTS.UPDATED_AT) AS UPDATED_AT
-FROM LISTINGS
-LEFT JOIN HOSTS ON (LISTINGS.HOST_ID = HOSTS.HOST_ID)
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    l.host_id,
+    h.host_name,
+    h.is_superhost AS host_is_superhost,
+    l.created_at,
+    GREATEST(l.updated_at, h.updated_at) AS updated_at
+FROM l
+LEFT JOIN h ON (l.host_id = h.host_id)
